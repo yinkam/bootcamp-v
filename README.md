a977b9b6b299587611ed1453a59edd891aa59d73
yinka <yinka.makanjuola@gmail.com>
2015-02-20 15:33:18 +0100

    andelabs done

--- README.md
+++
@@ -1,9 +0,0 @@
-# string-splosion-lab
-
-Study the pattern below and write your program to pass the tests 
-
-```
- phone  => pphphophonphone
- ab     => aab
- like   => lliliklike
-```
--- chart-app-project/script.js
+++ chart-app-project/script.js
@@ -64,7 +64,12 @@ function itemlist() {
 	    list.appendChild(input_value);            
 	}           		
 }
-	
+
+function reset(){
+	var canvas = document.getElementById('chart');
+	var context = canvas.getContext('2d');
+	context.clearRect(0, 0, canvas.width, canvas.height);
+}
          
 //function to plot bar chat			  
 function barchart(dataset) {
@@ -77,8 +82,11 @@ function barchart(dataset) {
 	for(var s in dataset){			    
 		context.beginPath();
         context.rect(startx,500, 30,-dataset[s].percent*10); //multiply by 10 to improve scaling
-		context.fillStyle = '#F44336';
+		context.fillStyle = '#B71C1C';
 		context.fill();
+		context.font = '10pt Helvetica';
+		context.fillStyle = 'black';
+    	context.fillText(dataset[s].name, startx, 520);
 		startx += 50;
 	}			        
 }
@@ -87,21 +95,21 @@ function barchart(dataset) {
 function piechart(dataset) {
 	var canvas = document.getElementById('chart');
 	var context = canvas.getContext('2d');
-	var chartcolors = ['#F44336','#9C27B0', '#3F51B5', '#03A9F4', '#009688', 
-						'#8BC34A', '#FFEB3B', '#FF9800', '#795548', '#607D8B'];
-	var x = canvas.width/2;
-	var y = canvas.height/2;
+	var chartcolors = ['#B71C1C','#4A148C', '#1A237E', '#01579B', '#004D40', 
+						'#33691E', '#F57F17', '#E65100', '#3E2723', '#263238'];
+	var x = (canvas.width/2)-50;
+	var y = (canvas.height/2)-50;
 	var radius = y-100;	
 	var startAngle = 0;
     var title = document.getElementById('title').value;
+    var starty = 50;
               
     context.clearRect(0, 0, canvas.width, canvas.height);
               
     context.font = '20pt Helvetica';
-    context.fillText(title, canvas.width/2-50, 50);
+    context.fillText(title, x-100, 50);
 
-	//convert percentage in radians and plot graph	
-				
+	//convert percentage in radians and plot graph				
 	for(var s in dataset){
 		dataset[s].radian = (dataset[s].percent/100)*Math.PI*2;
 		context.fillStyle = chartcolors[s];
@@ -109,7 +117,17 @@ function piechart(dataset) {
 		context.arc(x, y, radius, startAngle,startAngle+dataset[s].radian, false);
 		context.lineTo(x,y);
 		context.fill();
+
+		//create Legend
+		context.rect(550,starty, 15,15);
+		context.fillStyle = chartcolors[s];
+		context.fill();
+
+		context.font = '10pt Helvetica';
+		context.fillStyle = 'black';
+    	context.fillText(dataset[s].name, 570, starty+12);
 		startAngle += dataset[s].radian;
+		starty+=20;
 	}    
 }
           
@@ -118,18 +136,22 @@ function piechart(dataset) {
 function linechart(dataset) {
 	var canvas = document.getElementById('chart');
 	var context = canvas.getContext('2d');
-	var startx = 50;
-							
+	var startx = 50;							
 	context.clearRect(0, 0, canvas.width, canvas.height);
 	plotaxis();
 	context.beginPath();
 				
 	//convert percentage in radians and plot graph			       
-	for(var s in dataset){	   
+	for(var s in dataset){	
+		//create line   
 		context.lineTo(startx, (500-dataset[s].percent*5));//y=axis numbers multiplied by 5 to improve scaling
 		context.lineWidth = 3;
 		context.strokeStyle = '#F44336';
 		context.stroke();
+		//create Labels
+		context.font = '10pt Helvetica';
+		context.fillStyle = 'black';
+    	context.fillText(dataset[s].name, startx, 520);
 		startx += 50;
 	}	
 }
@@ -142,7 +164,8 @@ function plotaxis(){
     var title = document.getElementById('title').value;
 	context.clearRect(0, 0, canvas.width, canvas.height);              	
     context.font = '20pt Helvetica';
-    context.fillText(title, canvas.width/2-50, 50);
+    context.fillStyle = 'black';
+    context.fillText(title, canvas.width/2-100, 50);
 	context.beginPath();
 	context.moveTo(startx, 100);
 	context.lineTo(startx, 500);
